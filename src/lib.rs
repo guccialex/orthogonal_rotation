da7ccc4234a51c9301cd4e6c4ab46936cf01bcd4
guccialex <alex.gusain@gmail.com>
2021-03-29 20:31:12 -0400

    added support for rotating i8s round nonwhole ints

--- src/lib.rs
+++ src/lib.rs
@@ -62,25 +62,34 @@ fn print_2d_vector(vec: Vec<Vec<u32>>){
 //turn the point into 
 pub fn ortho_rotate_i8_point(point: (i8,i8), torotate: f32 ) -> (i8,i8){
     
-    let floatpoint = (point.0 as f32, point.1 as f32);
-    
-    let rotatedfloatpoint = ortho_rotate_point_at_origin(floatpoint, torotate);
-    
-    let rotatedi8point = (rotatedfloatpoint.0.round() as i8, rotatedfloatpoint.1.round() as i8);
-    
-    
-    return rotatedi8point;
+    return ortho_rotate_i8_point_at_point(point, (0,0), torotate);
 }
 
 
 pub fn ortho_rotate_i8_point_at_point(point: (i8,i8), origin: (i8,i8), torotate: f32 ) -> (i8,i8){
     
-    let pointprime = (point.0 - origin.0, point.1 - origin.1);
+    let floatpoint = i8_pos_to_float_pos(point);
+    let floatorigin = i8_pos_to_float_pos(origin);
     
-    let rotatedpoint = ortho_rotate_i8_point(pointprime, torotate);
+    let rotatedpoint = ortho_rotate_point_at_point(floatpoint, floatorigin, torotate);
     
-    let rotatedpointprime = (rotatedpoint.0 + origin.0, rotatedpoint.1 + origin.1);
+    let rotatedpointprime = float_pos_to_i8_pos(rotatedpoint);
+
+    return rotatedpointprime;
+}
+
+
+//rotate a point where the center of it is the bottom left of the point specified
+//used so that the origin (4,4) rotates the pieces on a chess board around the center
+pub fn ortho_rotate_i8_point_at_bot_left_of_point(point: (i8,i8), origin: (i8,i8), torotate: f32 ) -> (i8,i8){
     
+    let floatpoint = i8_pos_center_to_float_pos(point);
+    let floatorigin = i8_pos_to_float_pos(origin);
+    
+    let rotatedpoint = ortho_rotate_point_at_point(floatpoint, floatorigin, torotate);
+    
+    let rotatedpointprime = float_pos_to_i8_pos(rotatedpoint);
+
     return rotatedpointprime;
 }
 
@@ -88,11 +97,10 @@ pub fn ortho_rotate_i8_point_at_point(point: (i8,i8), origin: (i8,i8), torotate:
 //rotate a point with another point as its origin
 pub fn ortho_rotate_point_at_point( point: (f32,f32), originpoint: (f32,f32), torotate: f32) -> (f32,f32){
     
-    
     //move the point so that the origin is the originpoint
     let pointprime = (point.0 - originpoint.0, point.1 - originpoint.1);
     
-    let pointprime2 = ortho_rotate_point_at_origin(point, torotate);
+    let pointprime2 = ortho_rotate_point_at_origin(pointprime, torotate);
     
     let pointprime3 = (pointprime2.0 + originpoint.0, pointprime2.1 + originpoint.1);
     
@@ -191,4 +199,24 @@ fn rotate_point_90_degrees_x_times( mut point: (f32,f32), mut times: i8 ) -> (f3
     
     
     return point;
+}
+
+
+
+fn i8_pos_to_float_pos( i8pos: (i8,i8) ) -> (f32,f32){
+
+    return ( i8pos.0 as f32 , i8pos.1 as f32  ) ;
+}
+
+//if it is considered a square the size of 1 unit with its bottom left at i8pos
+//return the center of that (would be its i8 value +0.5)
+fn i8_pos_center_to_float_pos( i8pos: (i8,i8) ) -> (f32,f32){
+
+    return ( i8pos.0 as f32 + 0.5, i8pos.1 as f32 + 0.5);
+}
+
+
+fn float_pos_to_i8_pos( floatpos: (f32,f32) ) -> (i8,i8){
+
+    return ( (floatpos.0).round() as i8, (floatpos.1).round() as i8 ) ;
 }
